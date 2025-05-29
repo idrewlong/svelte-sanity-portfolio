2d5e98f02c3515cfa76b3fe20442e3f6a94212f8
Richard Andrew Long <idrewlong@gmail.com>
2025-05-28 19:46:05 -0500

    draft

---
+++ src/lib/components/HeroSection.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+</script>
+
+<section class="hero-section default-margin mt-l">
+	<h3 class="mb-s">Hello, I'm Andrew</h3>
+	<div class="underscore mb-m"></div>
+	<h1>I'm a Full Stack developer crafting intuitive and performant</h1>
+	<h1 class="dark-grey">web experiences</h1>
+	<button>Let's Talk</button>
+</section>
+
+<style>
+	.hero-section {
+		padding-top: 60px;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import { HeroSection } from '$lib';
+</script>
+
+<HeroSection />
